286837ae5df36ade49da0d75273cb1c31968e841
thomasinfor <thomaswang2003@gmail.com>
2022-10-11 15:59:27 +0800

    improve submit.py

--- submit.py
+++ submit.py
@@ -1,16 +1,17 @@
 #! /bin/python3
 import os, subprocess, re
 from argparse import ArgumentParser
+from utils import split, squeeze
 
 parser = ArgumentParser()
-parser.add_argument("--time", help="wait response time", default=0.1, type=float)
-parser.add_argument("-t", help="specify tasks", default=None, nargs="+")
-parser.add_argument("--name", help="username ([a-zA-Z0-9_]+), make it short", required=True)
-parser.add_argument("--mk", help="make", action='store_true')
-parser.add_argument("--run", help="run all tasks", action='store_true')
-parser.add_argument("--sum", help="summary", action='store_true')
-parser.add_argument("--submit", help="submit mode", action='store_true')
-parser.add_argument("-a", help="--run + --sum + --submit", action='store_true')
+parser.add_argument("--time", default=0.1, type=float, help="wait response time (default 0.1)")
+parser.add_argument("-t", default=None, nargs="+", help="specify tasks")
+parser.add_argument("--name", required=True, help="username ([a-zA-Z0-9_]+), make it short")
+parser.add_argument("--mk", action='store_true', help="make")
+parser.add_argument("--run", action='store_true', help="run all tasks")
+parser.add_argument("--sum", action='store_true', help="summary")
+parser.add_argument("--teacher", default=False, nargs='?', const=None, help="compare your output with whose? (leave blank for auto detection)")
+parser.add_argument("-a", action='store_true', help="--mk + --run + --sum")
 args = parser.parse_args()
 
 TIME_LIMIT = args.time
@@ -46,6 +47,7 @@ def test():
     for t in tasks:
         with open(rel_here(f'submissions/{user}--{t}.out'), 'w+') as f:
             execute(['python3', rel_here(f'test-{t}.py'), '-t', f'{TIME_LIMIT}'], stdout=f)
+leader = dict()
 def summary():
     lst = os.listdir(rel_here('submissions'))
     lst = [i[:-4] for i in lst if i.endswith('.out') and i.find('--') != -1]
@@ -65,10 +67,38 @@ def summary():
                 else:
                     d[s] = [u]
         rk = sorted(list(d.values()), key=lambda x: -len(x))
+        if rk: leader[t] = rk[0][0]
         for i in rk:
             c = green if len(i) == len(rk[0]) else gray
             print(c('\t'.join([str(len(i))] + i)))
+def teach():
+    for t in tasks:
+        fa = f'submissions/{user}--{t}.out'
+        if not exists(rel_here(fa)): continue
+        teacher = args.teacher if args.teacher != None else leader[t]
+        fb = f'submissions/{teacher}--{t}.out'
+        if not exists(rel_here(fb)): continue
+        with open(rel_here(fb), 'r') as f:
+            T = f.read()
+        with open(rel_here(fa), 'r') as f:
+            U = f.read()
+        if T == U: continue
+        print(f'{red(t)}:\t{green(teacher)} vs {gray(user)}')
+        T = split(T)
+        U = split(U)
+        n = min(len(T), len(U))
+        if T[:n] == U[:n]:
+            pass
+        else:
+            for i in range(n):
+                if T[i] != U[i]:
+                    break
+            U = [x for x in U[:i+1] if x[0] == U[i][0]]
+            print(cyan(f'Difference found at step {i} (id = {T[i][0]})'))
+            print(gray(squeeze(U)), end='')
+            print(green(T[i][1][T[i][1].find('\n')+1:]))
 
 if args.mk or args.a: make()
 if args.run or args.a: test()
-if args.sum or args.a: summary()
+if args.teacher != False or args.sum or args.a: summary()
+if args.teacher != False or args.a: teach()
